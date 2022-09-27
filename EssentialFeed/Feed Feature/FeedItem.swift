b0dd310529bbf6da86aca74c7541dc257ac32897
Long Hoàng <longhoang.2984@gmail.com>
2022-09-27 23:22:34 +0700

    Create The FeedItem model

---
+++ EssentialFeed/Feed Feature/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Cửu Long Hoàng on 27/09/2022.
+//
+
+import Foundation
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+}
