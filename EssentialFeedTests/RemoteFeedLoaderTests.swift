cde824952fac3cd3edb38c9167de7b958896dc4b
Long Hoàng <longhoang.2984@gmail.com>
2022-09-27 23:29:01 +0700

    `RemoteFeedLoader` does not request Feed Data

---
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -0,0 +1,28 @@
+//
+//  RemoteFeedLoaderTests.swift
+//  EssentialFeedTests
+//
+//  Created by Cửu Long Hoàng on 27/09/2022.
+//
+
+import XCTest
+@testable import EssentialFeed
+
+class RemoteFeedLoader {
+    
+}
+
+class HTTPClient {
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient()
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
+    }
+
+}
