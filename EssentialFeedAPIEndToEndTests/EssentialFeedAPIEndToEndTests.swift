54926b205a23d8bcc88f4657c3287888510dd5ac
Long Hoàng <longhoang.2984@gmail.com>
2022-10-01 01:24:57 +0700

    Add end-to-end API tests

---
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -0,0 +1,95 @@
+//
+//  EssentialFeedAPIEndToEndTests.swift
+//  EssentialFeedAPIEndToEndTests
+//
+//  Created by Cửu Long Hoàng on 01/10/2022.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedAPIEndToEndTests: XCTestCase {
+    
+    func test_endToEndTestServerGETFeedResult_matchesFixedTestAccountData() {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
+        let client = URLSessionHTTPClient()
+        let loader = RemoteFeedLoader(url: testServerURL, client: client)
+        
+        var receivedResult: LoadFeedResult?
+        let exp = expectation(description: "Wait for loader completion")
+        loader.load { result in
+            receivedResult = result
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 5.0)
+        
+        switch receivedResult {
+        case .success(let items)?:
+            XCTAssertEqual(items.count, 8)
+            XCTAssertEqual(items[0], expectedImage(at: 0))
+            XCTAssertEqual(items[1], expectedImage(at: 1))
+            XCTAssertEqual(items[2], expectedImage(at: 2))
+            XCTAssertEqual(items[3], expectedImage(at: 3))
+            XCTAssertEqual(items[4], expectedImage(at: 4))
+            XCTAssertEqual(items[5], expectedImage(at: 5))
+            XCTAssertEqual(items[6], expectedImage(at: 6))
+            XCTAssertEqual(items[7], expectedImage(at: 7))
+        case .failure(let error)?:
+            XCTFail("Expect receive success, got \(error) instead")
+        default:
+            XCTFail("Expect receive success, got no result instead")
+        }
+    }
+    
+    private func expectedImage(at index: Int) -> FeedItem {
+        return FeedItem(
+            id: id(at: index),
+            description: description(at: index),
+            location: location(at: index),
+            imageURL: imageURL(at: index))
+    }
+    
+    private func id(at index: Int) -> UUID {
+        return UUID(uuidString: [
+            "73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6",
+            "BA298A85-6275-48D3-8315-9C8F7C1CD109",
+            "5A0D45B3-8E26-4385-8C5D-213E160A5E3C",
+            "FF0ECFE2-2879-403F-8DBE-A83B4010B340",
+            "DC97EF5E-2CC9-4905-A8AD-3C351C311001",
+            "557D87F1-25D3-4D77-82E9-364B2ED9CB30",
+            "A83284EF-C2DF-415D-AB73-2A9B8B04950B",
+            "F79BD7F8-063F-46E2-8147-A67635C3BB01"
+        ][index])!
+    }
+    
+    private func description(at index: Int) -> String? {
+        return [
+            "Description 1",
+            nil,
+            "Description 3",
+            nil,
+            "Description 5",
+            "Description 6",
+            "Description 7",
+            "Description 8"
+        ][index]
+    }
+    
+    private func location(at index: Int) -> String? {
+        return [
+            "Location 1",
+            "Location 2",
+            nil,
+            nil,
+            "Location 5",
+            "Location 6",
+            "Location 7",
+            "Location 8"
+        ][index]
+    }
+    
+    private func imageURL(at index: Int) -> URL {
+        return URL(string: "https://url-\(index+1).com")!
+    }
+    
+}
