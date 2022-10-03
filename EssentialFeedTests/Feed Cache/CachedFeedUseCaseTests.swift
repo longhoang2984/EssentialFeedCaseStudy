6f235ade9500aeaffb8354ed2852f42b44440b3a
Long Hoàng <longhoang.2984@gmail.com>
2022-10-03 22:13:37 +0700

    Extract Cached Insertion as a helper method

--- EssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
@@ -31,11 +31,15 @@ class LocalFeedLoader {
             if let error = error {
                 completion(error)
             } else {
-                self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
-                    guard self != nil else { return }
-                    completion(error)
-                }
+                self.cache(items, completion: completion)
             }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
         }
     }
 }
