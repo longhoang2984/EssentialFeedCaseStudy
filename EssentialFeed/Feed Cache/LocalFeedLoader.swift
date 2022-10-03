07ecc866d5bd8989c4a95f9e10482c90dae791f0
Long Hoàng <longhoang.2984@gmail.com>
2022-10-03 22:16:14 +0700

    Create `FeedStore` own file

---
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,36 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Cửu Long Hoàng on 03/10/2022.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    private let store: FeedStore
+    private let currentDate: () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            if let error = error {
+                completion(error)
+            } else {
+                self.cache(items, completion: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
