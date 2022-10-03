a8c114af0dafa31ceb06a4ac59c86879a64f0c6e
Long Hoàng <longhoang.2984@gmail.com>
2022-10-03 18:51:07 +0700

    Delete cached feed when save new items

--- EssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CachedFeedUseCaseTests.swift
@@ -6,9 +6,14 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class FeedStore {
     var deleteCachedFeedCallCount = 0
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
+    }
 }
 
 class LocalFeedLoader {
@@ -16,6 +21,10 @@ class LocalFeedLoader {
     
     init(store: FeedStore) {
         self.store = store
+    }
+    
+    func save(_ items: [FeedItem]) {
+        store.deleteCachedFeed()
     }
 }
 
@@ -25,10 +34,27 @@ final class CachedFeedUseCaseTests: XCTestCase {
         
         let store = FeedStore()
         
-        let sut = LocalFeedLoader(store: store)
+        _ = LocalFeedLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+    func test_save_requestCacheDeletion() {
+        let items = [uniqueItem(), uniqueItem()]
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        sut.save(items)
         
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
+    
+    // MARK: - Helpers
+    func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), description: "desc", location: "location", imageURL: anyURL())
+    }
+    
+    func anyURL() -> URL {
+        return URL(string: "https://url.com")!
     }
 
 }
