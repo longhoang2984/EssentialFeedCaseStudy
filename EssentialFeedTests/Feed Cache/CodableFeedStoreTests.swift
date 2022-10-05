6ce89628bdc72fdc4b069348a13df7b5ca4e11f0
Long Hoàng <longhoang.2984@gmail.com>
2022-10-06 01:31:48 +0700

    Retrieving after inserting to empty cache
    Retrieving after inserting to empty cache delivers inserted values

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -9,8 +9,29 @@ import XCTest
 import EssentialFeed
 
 private class CodableFeedStore {
+    
+    private struct Cache: Codable {
+        let feed: [LocalFeedImage]
+        let timestamp: Date
+    }
+    
+    private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
-        completion(.empty)
+        guard let data = try? Data(contentsOf: storeURL) else {
+            return completion(.empty)
+        }
+        
+        let decoder = JSONDecoder()
+        let decoded = try! decoder.decode(Cache.self, from: data)
+        completion(.found(feed: decoded.feed, timestamp: decoded.timestamp))
+    }
+    
+    func insert(_ feeds: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
+        let encoder = JSONEncoder()
+        let encoded = try! encoder.encode(Cache(feed: feeds, timestamp: timestamp))
+        try! encoded.write(to: storeURL)
+        completion(nil)
     }
 }
 
@@ -43,6 +64,28 @@ final class CodableFeedStoreTests: XCTestCase {
                     break
                 default:
                     XCTFail("Expect retrieving twice from empty cache to deliver same empty result, got \(firstResult) \(secondResult) instead")
+                }
+                exp.fulfill()
+            }
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
+        let sut = CodableFeedStore()
+        let feed = uniqueFeeds()
+        let timestamp = Date()
+    
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.insert(feed.local, timestamp: timestamp) { insertError in
+            XCTAssertNil(insertError)
+            sut.retrieve { result in
+                switch result {
+                case let .found(retrievedFeed, retrievedTimestamp):
+                    XCTAssertEqual(retrievedFeed, feed.local)
+                    XCTAssertEqual(retrievedTimestamp, timestamp)
+                default:
+                    XCTFail("Expect found result with feed \(feed) and timestamp \(timestamp), got \(result) instead")
                 }
                 exp.fulfill()
             }
