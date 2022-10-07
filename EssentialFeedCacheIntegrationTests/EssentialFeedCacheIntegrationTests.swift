7c2ae5fa877ce26b5b603ac32ad7a76e7c837f22
Long Hoàng <longhoang.2984@gmail.com>
2022-10-08 00:33:09 +0700

    Add `EssentialFeedCacheIntegrationTests`

---
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialFeedCacheIntegrationTests.swift
+//  EssentialFeedCacheIntegrationTests
+//
+//  Created by Cửu Long Hoàng on 08/10/2022.
+//
+
+import XCTest
+import EssentialFeed
+
+final class EssentialFeedCacheIntegrationTests: XCTestCase {
+    
+}
