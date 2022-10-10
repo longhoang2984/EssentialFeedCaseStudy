d1f5984399f50ed902ae712e67de3a4b7993395a
Long Hoàng <longhoang.2984@gmail.com>
2022-10-10 23:12:53 +0700

    Render loaded images from URL

--- EssentialFeediOS/FeedImageCell.swift
+++ EssentialFeediOS/FeedImageCell.swift
@@ -12,4 +12,5 @@ public final class FeedImageCell: UITableViewCell {
     public let locationLabel: UILabel = UILabel()
     public let descriptionLabel: UILabel = UILabel()
     public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
 }
