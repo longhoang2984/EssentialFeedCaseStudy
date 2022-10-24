c65e9cdb6024cebcb3e9df3b2e24cfd1ac71bac8
Long Hoàng <longhoang.2984@gmail.com>
2022-10-24 20:15:55 +0700

    rename `FeedViewControllerTest` to `FeedUIIntegrationTests`

--- EssentialFeediOSTests/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/FeedUIIntegrationTests.swift
@@ -10,7 +10,7 @@ import UIKit
 import EssentialFeed
 import EssentialFeediOS
 
-final class FeedViewControllerTests: XCTestCase {
+final class FeedUIIntegrationTests: XCTestCase {
     
     func test_feedContent_withTitle() {
         let (sut, _) = makeSUT()
