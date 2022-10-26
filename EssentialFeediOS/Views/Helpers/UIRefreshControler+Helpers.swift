fa785204d267f6ef75662c180eec4290dfaa03a5
Long Hoàng <longhoang.2984@gmail.com>
2022-10-26 23:21:00 +0700

    Extract refreshing logic to an extension

---
+++ EssentialFeediOS/Views/Helpers/UIRefreshControler+Helpers.swift
@@ -0,0 +1,14 @@
+//
+//  UIRefreshControler+Helpers.swift
+//  EssentialFeediOS
+//
+//  Created by Cửu Long Hoàng on 26/10/2022.
+//
+
+import UIKit
+
+extension UIRefreshControl {
+    func update(isRefreshing: Bool) {
+        isRefreshing ? beginRefreshing() : endRefreshing()
+    }
+}
