2ca981635cfd2dd44dba2ac291c7b92a9ba9465f
Long Hoàng <longhoang.2984@gmail.com>
2022-10-27 01:09:00 +0700

    Move `FeedErrorViewModel` to separate file

--- EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -5,18 +5,6 @@
 //  Created by Cửu Long Hoàng on 27/10/2022.
 //
 
-public struct FeedErrorViewModel {
-    public var message: String?
-    
-    static public var noError: FeedErrorViewModel {
-        return FeedErrorViewModel(message: nil)
-    }
-    
-    static public func error(message: String) -> FeedErrorViewModel {
-        return FeedErrorViewModel(message: message)
-    }
-}
-
 public protocol FeedView {
     func display(_ viewModel: FeedViewModel)
 }
