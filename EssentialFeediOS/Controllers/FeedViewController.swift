f9c661a28ffbef089bf9c0f314aed6288693acf5
Long Hoàng <longhoang.2984@gmail.com>
2022-10-26 22:56:14 +0700

    Move `ErrorView` to separate file

--- EssentialFeediOS/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Controllers/FeedViewController.swift
@@ -11,10 +11,6 @@ protocol FeedViewControllerDelegate {
     func didRequestFeedRefresh()
 }
 
-public final class ErrorView: UIView {
-    public var message: String?
-}
-
 public final class FeedViewController: UITableViewController, FeedLoadingView, UITableViewDataSourcePrefetching, FeedErrorView {
     var delegate: FeedViewControllerDelegate?
     @IBOutlet public var errorView: ErrorView?
---
+++ EssentialFeediOS/Views/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Cửu Long Hoàng on 26/10/2022.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
