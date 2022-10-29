a0d521773a0a89fbc24f7f0e28af3b361d0553b5
Long Hoàng <longhoang.2984@gmail.com>
2022-10-30 00:02:08 +0700

    Add test to guarantee that `CoreDataFeedStore` side effects run serially to prevent unexpected behavior

--- EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedImageDataStoreTests.swift
@@ -48,6 +48,22 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
         expect(sut, toCompleteRetrievalWith: found(lastStoredData), for: url)
     }
     
+    func test_sideEffects_runSerially() {
+        let sut = makeSUT()
+        let url = anyURL()
+        
+        let op1 = expectation(description: "Operation 1")
+        sut.insert([localImage(url: url)], timestamp: Date()) { _ in op1.fulfill() }
+        
+        let op2 = expectation(description: "Operation 2")
+        sut.insert(anyData(), for: url, completion: { _ in op2.fulfill() })
+        
+        let op3 = expectation(description: "Operation 3")
+        sut.insert(anyData(), for: url, completion: { _ in op3.fulfill() })
+        
+        wait(for: [op1, op2, op3], timeout: 5.0, enforceOrder: true)
+    }
+    
     // MARK: - Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CoreDataFeedStore {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
@@ -98,9 +114,9 @@ final class CoreDataFeedImageDataStoreTests: XCTestCase {
                     if case let Result.failure(error) = result {
                         XCTFail("Failed to insert \(data) with error \(error)", file: file, line: line)
                     }
+                    exp.fulfill()
                 }
             }
-            exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
     }
