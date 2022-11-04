c75cac6a9830d0cd89698687f34a7ca8ab8a1abc
Long Hoàng <longhoang.2984@gmail.com>
2022-11-04 19:46:12 +0700

    Add EssentialApp

---
+++ EssensialApp/EssensialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssensialApp
+//
+//  Created by Cửu Long Hoàng on 27/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
