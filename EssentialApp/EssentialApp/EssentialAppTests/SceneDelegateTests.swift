030e4ea7b9933cb1f57bb0e0747fb5571cf48961
Long Hoàng <longhoang.2984@gmail.com>
2022-11-18 19:25:40 +0700

    Remove unnecessary main storyboard

--- EssentialApp/EssentialApp/EssentialAppTests/SceneDelegateTests.swift
+++ EssentialApp/EssentialApp/EssentialAppTests/SceneDelegateTests.swift
@@ -11,6 +11,18 @@ import EssentialFeediOS
 
 final class SceneDelegateTests: XCTestCase {
 
+    func test_configureWindow_setsWindowAsKeyAndVisible() {
+        let window = UIWindow()
+        window.windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+        let sut = SceneDelegate()
+        sut.window = window
+        
+        sut.configureWindow()
+        
+        XCTAssertTrue(window.isKeyWindow, "Expected window to be the key window")
+        XCTAssertFalse(window.isHidden, "Expected window to be visible")
+    }
+    
     func test_appLaunch_displayFeedViewController() {
         
         let scene = SceneDelegate()
